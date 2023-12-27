952e0633de5c9bedcfb6e2bf1001cd3b4575272b
Sebastian Boroń <sekabo.skb@gmail.com>
2023-12-27 16:24:43 +0100

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'blue123': '#1fb8ff',
+			}
+		},
+	},
+	plugins: [],
+}
